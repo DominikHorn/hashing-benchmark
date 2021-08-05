963434d44f17a8a1992897a3419d6d0888bbcf01
Dominik Horn <dominik.horn@tum.de>
2021-08-05 20:02:57 +0200

    add arguments/templating to kapil benchmarks

--- src/kapil_benchmarks.hpp
+++ src/kapil_benchmarks.hpp
@@ -8,65 +8,101 @@
 #include <random>
 #include <vector>
 
-static void BM_SortedArrayRangeLookup(benchmark::State& state) {
-  using Key = std::uint64_t;
-  using Payload = std::uint64_t;
-  struct Slot {
-    Key key = std::numeric_limits<Key>::max();
-    Payload payload = 0;
+namespace _kapil_benchmark {
+using Key = std::uint64_t;
+using Payload = std::uint64_t;
 
-    bool operator<(const Slot& rhs) const { return this->key < rhs.key; }
-    bool operator<(const Key& rhs) const { return this->key < rhs; }
-  } __attribute__((packed));
+const size_t dataset_size = 100000;
+const size_t interval_min = 1;
+const size_t interval_max = dataset_size / 2;
+const size_t dataset_min = 1;
+const size_t dataset_max = 2;
 
-  std::vector<Slot> dataset(1000000);
+std::random_device rd;
+std::default_random_engine rng(rd());
 
-  // Random generator
-  std::random_device rd;
-  std::default_random_engine rng(rd());
+static std::vector<Key> get_dataset(size_t ind) {
+  // TODO: implement SOSD datasets
+  static std::vector<Key> ds_uniform_random;
+  static std::vector<Key> ds_sequential;
 
-  // Generate dataset with random holes
-  // TODO: dataset should be a variable benchmark parameter
-  // TODO: use different datasets
-  {
-    std::uniform_int_distribution<size_t> dist(0, 99);
-    for (size_t idx = 0, num = 0; idx < dataset.size(); idx++, num++) {
-      while (dist(rng) < 90) num++;
-      dataset[idx].key = num;
-      dataset[idx].payload = num - 1;
-    }
+  switch (ind) {
+    case 1:
+      if (ds_uniform_random.empty()) {
+        ds_uniform_random.resize(dataset_size);
+        std::uniform_int_distribution<size_t> dist(0, 99);
+        for (size_t idx = 0, num = 0; idx < ds_uniform_random.size();
+             idx++, num++) {
+          while (dist(rng) < 90) num++;
+          ds_uniform_random[idx] = num;
+        }
+      }
+      return ds_uniform_random;
+    case 2:
+      if (ds_sequential.empty()) {
+        ds_sequential.resize(dataset_size);
+        Key k = 2000;
+        for (size_t i = 0; i < ds_sequential.size(); i++, k++)
+          ds_sequential[i] = k;
+      }
+      return ds_sequential;
   }
 
-  // Dataset is already sorted by construction, however,
-  // this will not be the case for all datasets in the future
-  std::sort(dataset.begin(), dataset.end());
+  // TODO: default error (?)
+  assert(false);
+  return {};
+}
+
+static void BM_SortedArrayRangeLookupBinarySearch(benchmark::State& state) {
+  const size_t interval_size = state.range(0);
+  const size_t dataset_ind = state.range(1);
+
+  const auto dataset = get_dataset(dataset_ind);
 
   // Random lookups
-  const auto min_key = dataset[0].key;
-  const auto max_key = dataset[dataset.size() - 1].key;
+  const auto min_key = dataset[0];
+  const auto max_key = dataset[dataset.size() - 1];
   std::uniform_int_distribution<size_t> dist(min_key, max_key);
-  // TODO: interval_size should be a benchmark parameter
-  // TODO: use different interval sizes
-  const size_t interval_size = 1000;
-
-  const auto lower_bound = [&](const Key& val) {
-    // TODO: use implementation other than (probably) binary search, e.g.,
-    // linear interpolation search, exponential search etc
-    return std::lower_bound(dataset.begin(), dataset.end(), val);
-  };
 
   for (auto _ : state) {
     const auto lower = dist(rng);
     const auto upper = lower + interval_size;
 
     std::vector<Payload> result;
-    for (auto iter = lower_bound(lower);
+    for (auto iter = std::lower_bound(dataset.begin(), dataset.end(), lower);
          // TODO: use key <= upper?
-         iter < dataset.end() && iter->key < upper; iter++)
-      result.push_back(iter->payload);
+         iter < dataset.end() && *iter < upper; iter++)
+      result.push_back(*iter - 1);
 
     benchmark::DoNotOptimize(result.data());
   }
 }
 
-BENCHMARK(BM_SortedArrayRangeLookup);
+template <size_t SecondLevelModelCount>
+static void BM_SortedArrayRangeLookupRMI(benchmark::State& state) {
+  for (auto _ : state) {
+    // TODO: implement
+  }
+}
+
+BENCHMARK(BM_SortedArrayRangeLookupBinarySearch)
+    ->RangeMultiplier(2)
+    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+
+BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 10)
+    ->RangeMultiplier(2)
+    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 100)
+    ->RangeMultiplier(2)
+    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 1000)
+    ->RangeMultiplier(2)
+    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 100000)
+    ->RangeMultiplier(2)
+    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 1000000)
+    ->RangeMultiplier(2)
+    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+}  // namespace _kapil_benchmark
+
