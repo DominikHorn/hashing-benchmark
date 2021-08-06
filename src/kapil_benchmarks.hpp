aca4f61a179ecd9b24edca1106e71c224793d354
Dominik Horn <dominik.horn@tum.de>
2021-08-06 19:00:55 +0200

    implement new benchmarks
    * add missing SOSD datasets
    * add sorting/copying baselines (rmi buckets don't need to sort!)
    * construct all RMIs on a 1% sample
    * improve benchmark argument handling
    * fix memory access errors

--- src/kapil_benchmarks.hpp
+++ src/kapil_benchmarks.hpp
@@ -10,54 +10,41 @@
 #include <random>
 #include <vector>
 
+#include "support/datasets.hpp"
+
 namespace _ {
 using Key = std::uint64_t;
 using Payload = std::uint64_t;
 
-const size_t dataset_size = 1000000;
-const size_t interval_min = 1;
-const size_t interval_max = dataset_size / 2;
-const size_t dataset_min = 1;
-const size_t dataset_max = 2;
+const std::vector<std::int64_t> intervals{1, 2, 4, 8, 16, 32, 64, 128, 256};
+const std::vector<std::int64_t> datasets{
+    Dataset::ID::SEQUENTIAL, Dataset::ID::GAPPED_10, Dataset::ID::UNIFORM,
+    Dataset::ID::FB,         Dataset::ID::OSM,       Dataset::ID::WIKI};
 
 std::random_device rd;
 std::default_random_engine rng(rd());
 
-static std::vector<Key> get_dataset(size_t ind) {
-  // TODO: implement SOSD datasets
-  static std::vector<Key> ds_uniform_random;
-  static std::vector<Key> ds_sequential;
-
-  switch (ind) {
-    case 1:
-      if (ds_uniform_random.empty()) {
-        ds_uniform_random.resize(dataset_size);
-        std::uniform_int_distribution<size_t> dist(0, 99);
-        for (size_t idx = 0, num = 0; idx < ds_uniform_random.size(); idx++) {
-          do num++;
-          while (dist(rng) < 90);
-          ds_uniform_random[idx] = num;
-        }
-      }
-      return ds_uniform_random;
-    case 2:
-      if (ds_sequential.empty()) {
-        ds_sequential.resize(dataset_size);
-        Key k = 2000;
-        for (size_t i = 0; i < ds_sequential.size(); i++, k++)
-          ds_sequential[i] = k;
-      }
-      return ds_sequential;
+static void BM_CopyArray(benchmark::State& state) {
+  for (auto _ : state) {
+    auto dataset = Dataset::load_cached(Dataset::ID::SEQUENTIAL);
+    benchmark::DoNotOptimize(dataset.data());
   }
+}
 
-  // TODO: default error (?)
-  assert(false);
-  return {};
+static void BM_SortArray(benchmark::State& state) {
+  for (auto _ : state) {
+    auto dataset = Dataset::load_cached(Dataset::ID::SEQUENTIAL);
+    std::sort(dataset.begin(), dataset.end());
+    benchmark::DoNotOptimize(dataset.data());
+  }
 }
 
 static void BM_SortedArrayRangeLookupBinarySearch(benchmark::State& state) {
   const size_t interval_size = state.range(0);
-  const auto dataset = get_dataset(state.range(1));
+  auto dataset = Dataset::load_cached(static_cast<Dataset::ID>(state.range(1)));
+
+  // We must sort the entire array ;(
+  std::sort(dataset.begin(), dataset.end());
 
   const auto min_key = dataset[0];
   const auto max_key = dataset[dataset.size() - 1];
@@ -70,7 +57,7 @@ static void BM_SortedArrayRangeLookupBinarySearch(benchmark::State& state) {
     std::vector<Payload> result;
     for (auto iter = std::lower_bound(dataset.begin(), dataset.end(), lower);
          iter < dataset.end() && *iter < upper; iter++)
-      result.push_back(*iter - 1);  // TODO: actual payload lookup?
+      result.push_back(*iter - 1);
 
     benchmark::DoNotOptimize(result.data());
   }
@@ -79,21 +66,29 @@ static void BM_SortedArrayRangeLookupBinarySearch(benchmark::State& state) {
 template <size_t SecondLevelModelCount>
 static void BM_SortedArrayRangeLookupRMI(benchmark::State& state) {
   const size_t interval_size = state.range(0);
-  const auto dataset = get_dataset(state.range(1));
+  const auto dataset =
+      Dataset::load_cached(static_cast<Dataset::ID>(state.range(1)));
 
   const auto min_key = dataset[0];
   const auto max_key = dataset[dataset.size() - 1];
   std::uniform_int_distribution<size_t> dist(min_key, max_key);
 
-  // determine maximum model error
+  // build model based on data sample. assume data is random shuffled (which it
+  // is) to compactify this code
+  std::vector<decltype(dataset)::value_type> sample(
+      dataset.begin(), dataset.begin() + dataset.size() / 100);
+  std::sort(sample.begin(), sample.end());
   const learned_hashing::RMIHash<Key, SecondLevelModelCount> rmi(
-      dataset.begin(), dataset.end(), dataset.size());
+      sample.begin(), sample.end(), dataset.size() - 1);
+
+  // determine maximum model error
   size_t max_error = 0;
   for (const auto& key : dataset) {
     const auto pred_ind = rmi(key);
     size_t actual_ind = pred_ind;
-    while (dataset[actual_ind] > key) actual_ind--;
-    while (dataset[actual_ind] < key) actual_ind++;
+    while (actual_ind > 0 && dataset[actual_ind] > key) actual_ind--;
+    while (actual_ind + 1 < dataset.size() && dataset[actual_ind] < key)
+      actual_ind++;
 
     max_error =
         std::max(max_error, pred_ind > actual_ind ? pred_ind - actual_ind
@@ -115,7 +110,7 @@ static void BM_SortedArrayRangeLookupRMI(benchmark::State& state) {
 
     for (auto iter = std::lower_bound(begin_iter, end_iter, lower);
          iter < dataset.end() && *iter < upper; iter++)
-      result.push_back(*iter - 1);  // TODO: actual payload lookup?
+      result.push_back(*iter - 1);
 
     benchmark::DoNotOptimize(result.data());
   }
@@ -125,7 +120,6 @@ const Key Sentinel = std::numeric_limits<Key>::max();
 template <size_t BucketSize>
 struct Bucket {
   std::array<Key, BucketSize> keys;
-  // std::array<Payload, BucketSize> payloads; // TODO: payloads?
 
   Bucket* next = nullptr;
 
@@ -138,7 +132,6 @@ struct Bucket {
     for (size_t i = 0; i < BucketSize; i++) {
       if (keys[i] == Sentinel) {
         keys[i] = key;
-        // b->payloads[i] = key-1; // TODO: payloads?
         return;
       }
     }
@@ -151,7 +144,8 @@ struct Bucket {
 template <size_t SecondLevelModelCount, size_t BucketSize>
 static void BM_BucketsRangeLookupRMI(benchmark::State& state) {
   const size_t interval_size = state.range(0);
-  const auto dataset = get_dataset(state.range(1));
+  const auto dataset =
+      Dataset::load_cached(static_cast<Dataset::ID>(state.range(1)));
 
   const auto min_key = dataset[0];
   const auto max_key = dataset[dataset.size() - 1];
@@ -160,10 +154,15 @@ static void BM_BucketsRangeLookupRMI(benchmark::State& state) {
   std::vector<Bucket<BucketSize>> buckets(
       dataset.size());  // TODO: load factors?
 
+  // build model based on data sample. assume data is random shuffled (which it
+  // is) to compactify this code
+  std::vector<decltype(dataset)::value_type> sample(
+      dataset.begin(), dataset.begin() + dataset.size() / 100);
+  std::sort(sample.begin(), sample.end());
   const learned_hashing::RMIHash<Key, SecondLevelModelCount> rmi(
-      dataset.begin(), dataset.end(), dataset.size());
+      sample.begin(), sample.end(), buckets.size() - 1);
 
-  // insert all keys
+  // insert all keys exactly where model tells us to
   for (const auto& key : dataset) buckets[rmi(key)].insert(key);
 
   for (auto _ : state) {
@@ -200,18 +199,15 @@ static void BM_BucketsRangeLookupRMI(benchmark::State& state) {
 
 #define __BENCHMARK_TWO_PARAM(fun, model_size, bucket_size) \
   BENCHMARK_TEMPLATE(fun, model_size, bucket_size)          \
-      ->RangeMultiplier(2)                                  \
-      ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+      ->ArgsProduct({intervals, datasets});
+
 #define _BENCHMARK_TWO_PARAM(fun, model_size) \
   __BENCHMARK_TWO_PARAM(fun, model_size, 1)   \
   __BENCHMARK_TWO_PARAM(fun, model_size, 2)   \
   __BENCHMARK_TWO_PARAM(fun, model_size, 4)   \
   __BENCHMARK_TWO_PARAM(fun, model_size, 8)   \
   __BENCHMARK_TWO_PARAM(fun, model_size, 16)  \
-  __BENCHMARK_TWO_PARAM(fun, model_size, 32)  \
-  __BENCHMARK_TWO_PARAM(fun, model_size, 64)  \
-  __BENCHMARK_TWO_PARAM(fun, model_size, 128) \
-  __BENCHMARK_TWO_PARAM(fun, model_size, 256)
+  __BENCHMARK_TWO_PARAM(fun, model_size, 32)
 #define BENCHMARK_TWO_PARAM(fun)     \
   _BENCHMARK_TWO_PARAM(fun, 10)      \
   _BENCHMARK_TWO_PARAM(fun, 100)     \
@@ -219,24 +215,23 @@ static void BM_BucketsRangeLookupRMI(benchmark::State& state) {
   _BENCHMARK_TWO_PARAM(fun, 1000000) \
   _BENCHMARK_TWO_PARAM(fun, 10000000)
 
+BENCHMARK(BM_CopyArray);
+BENCHMARK(BM_SortArray);
+
 BENCHMARK(BM_SortedArrayRangeLookupBinarySearch)
-    ->RangeMultiplier(2)
-    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+    ->ArgsProduct({intervals, datasets});
+
 BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 10)
-    ->RangeMultiplier(2)
-    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+    ->ArgsProduct({intervals, datasets});
 BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 100)
-    ->RangeMultiplier(2)
-    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+    ->ArgsProduct({intervals, datasets});
 BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 10000)
-    ->RangeMultiplier(2)
-    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+    ->ArgsProduct({intervals, datasets});
 BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 1000000)
-    ->RangeMultiplier(2)
-    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+    ->ArgsProduct({intervals, datasets});
 BENCHMARK_TEMPLATE(BM_SortedArrayRangeLookupRMI, 10000000)
-    ->RangeMultiplier(2)
-    ->Ranges({{interval_min, interval_max}, {dataset_min, dataset_max}});
+    ->ArgsProduct({intervals, datasets});
+
 BENCHMARK_TWO_PARAM(BM_BucketsRangeLookupRMI);
 }  // namespace _
 
