5eef7d609b03c03a02bf86e964c73c3445a01798
Dominik Horn <dominik.horn@tum.de>
2021-09-16 14:11:55 +0200

    export mcgraw & kapil server results to csv

--- results/sorted_rmi/export_results.py
+++ results/sorted_rmi/export_results.py
@@ -1,8 +1,12 @@
 import json
 import pandas as pd
 
-with open(f"../../benchmark_results.json") as data_file:
-    data = json.load(data_file)
-df = pd.json_normalize(data, 'benchmarks')
-df.to_csv(f"results.csv")
+def export_json_to_csv(file, result='results.csv'):
+    with open(file) as data_file:
+        data = json.load(data_file)
+        df = pd.json_normalize(data, 'benchmarks')
+        df.to_csv(result)
+
+export_json_to_csv("../../benchmark_results.json")
+export_json_to_csv("../../kapil_benchmark_results.json", 'kapil_results.csv')
 
