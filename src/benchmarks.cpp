b1cc407750d543fe898d284980fcaf2f4507ab54
Dominik Horn <dominik.horn@tum.de>
2021-09-23 02:29:01 +0200

    add code for naive benchmark target (e.g., for perfing)

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -1,37 +1,43 @@
 #include <benchmark/benchmark.h>
 
-// TODO(dominik): these imports merely serve to dogfood importing these
-// libraries (see if everything works)
-#include <cstdint>
-#include <exotic_hashing.hpp>
-
 #include "sorted_rmi.hpp"
-#include "support/datasets.hpp"
 
-// BENCHMARK_MAIN();
+BENCHMARK_MAIN();
 
-int main() {
-  auto dataset = dataset::load_cached(dataset::UNIFORM, 200000000);
-
-  std::random_device rd;
-  std::default_random_engine rng(rd());
-
-  // generate payloads
-  auto payloads = dataset;
-  std::shuffle(payloads.begin(), payloads.end(), rng);
-
-  // build hashtable
-  RMIHashtable<std::uint64_t, std::uint64_t, 1, 0> ht(dataset, payloads);
-
-  // benchmark
-  PerfEvent e;
-  e.startCounters();
-  for (size_t i = 0; i < dataset.size(); i++) {
-    const auto payload = ht.lookup(dataset[i]);
-    benchmark::DoNotOptimize(payload);
-  }
-  e.stopCounters();
-  e.printReport(std::cout, dataset.size());
-
-  return 0;
-}
+// #include <chrono>
+// #include <cstdint>
+//
+// #include "../include/rmi_hashtable.hpp"
+// #include "support/datasets.hpp"
+//
+// int main() {
+//   using T = std::uint32_t;
+//   auto dataset = dataset::load_cached<T>(dataset::ID::UNIFORM, 190000000);
+//
+//   std::random_device rd;
+//   std::default_random_engine rng(rd());
+//
+//   // build hashtable
+//   RMIHashtable<T, T, 8, 0> ht(dataset, dataset);
+//
+//   // probe order
+//   auto probe_order = dataset;
+//   std::shuffle(probe_order.begin(), probe_order.end(), rng);
+//
+//   for (size_t iter = 0; iter < 5; iter++) {
+//     size_t hits = 0;
+//     const auto start_time = std::chrono::steady_clock::now();
+//     for (size_t i = 0; i < probe_order.size(); i++) {
+//       hits += ht.lookup(probe_order[i]) == probe_order[i];
+//     }
+//     const auto end_time = std::chrono::steady_clock::now();
+//     const auto delta_ns = static_cast<uint64_t>(
+//         std::chrono::duration_cast<std::chrono::nanoseconds>(end_time -
+//                                                              start_time)
+//             .count());
+//     std::cout << static_cast<double>(delta_ns) /
+//                      static_cast<double>(dataset.size())
+//               << "; hits: " << hits << std::endl;
+//   }
+//   return 0;
+// }
