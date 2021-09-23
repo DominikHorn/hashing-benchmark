4954e074820696d2d8473b5477c9882e9984e523
Dominik Horn <dominik.horn@tum.de>
2021-09-23 02:25:48 +0200

    implement more SIMD specializations
    e.g., 32 bit keys or BucketSize 4

--- src/tests.cpp
+++ src/tests.cpp
@@ -5,13 +5,13 @@
 
 #include "../include/rmi_hashtable.hpp"
 
-using Key = std::uint64_t;
-using Payload = std::uint64_t;
-const std::vector<Key> keys{0, 1, 5, 8, 9, 10};
-const std::vector<Key> non_keys{2, 3, 4, 6, 7};
-const std::vector<Payload> payloads{3, 7, 13, 42, 69, 1337};
+TEST(RMIHashtable, Lookup1_32) {
+  using Key = std::uint32_t;
+  using Payload = std::uint32_t;
+  const std::vector<Key> keys{0, 1, 5, 8, 9, 10};
+  const std::vector<Key> non_keys{2, 3, 4, 6, 7};
+  const std::vector<Payload> payloads{3, 7, 13, 42, 69, 1337};
 
-TEST(RMIHashtable, Lookup1) {
   RMIHashtable<Key, Payload, 1, 100> ht(keys, payloads);
   for (size_t i = 0; i < keys.size(); i++)
     EXPECT_EQ(ht.lookup(keys[i]), payloads[i]);
@@ -20,7 +20,13 @@ TEST(RMIHashtable, Lookup1) {
     EXPECT_EQ(ht.lookup(non_keys[i]), std::numeric_limits<Payload>::max());
 }
 
-TEST(RMIHashtable, Lookup2) {
+TEST(RMIHashtable, Lookup2_32) {
+  using Key = std::uint32_t;
+  using Payload = std::uint32_t;
+  const std::vector<Key> keys{0, 1, 5, 8, 9, 10};
+  const std::vector<Key> non_keys{2, 3, 4, 6, 7};
+  const std::vector<Payload> payloads{3, 7, 13, 42, 69, 1337};
+
   RMIHashtable<Key, Payload, 2, 100> ht(keys, payloads);
   for (size_t i = 0; i < keys.size(); i++)
     EXPECT_EQ(ht.lookup(keys[i]), payloads[i]);
@@ -29,7 +35,88 @@ TEST(RMIHashtable, Lookup2) {
     EXPECT_EQ(ht.lookup(non_keys[i]), std::numeric_limits<Payload>::max());
 }
 
-TEST(RMIHashtable, Lookup8) {
+TEST(RMIHashtable, Lookup4_32) {
+  using Key = std::uint32_t;
+  using Payload = std::uint32_t;
+  const std::vector<Key> keys{0, 1, 5, 8, 9, 10};
+  const std::vector<Key> non_keys{2, 3, 4, 6, 7};
+  const std::vector<Payload> payloads{3, 7, 13, 42, 69, 1337};
+
+  RMIHashtable<Key, Payload, 4, 100> ht(keys, payloads);
+  for (size_t i = 0; i < keys.size(); i++)
+    EXPECT_EQ(ht.lookup(keys[i]), payloads[i]);
+
+  for (size_t i = 0; i < non_keys.size(); i++)
+    EXPECT_EQ(ht.lookup(non_keys[i]), std::numeric_limits<Payload>::max());
+}
+
+TEST(RMIHashtable, Lookup8_32) {
+  using Key = std::uint32_t;
+  using Payload = std::uint32_t;
+  const std::vector<Key> keys{0, 1, 5, 8, 9, 10};
+  const std::vector<Key> non_keys{2, 3, 4, 6, 7};
+  const std::vector<Payload> payloads{3, 7, 13, 42, 69, 1337};
+
+  RMIHashtable<Key, Payload, 8, 100> ht(keys, payloads);
+  for (size_t i = 0; i < keys.size(); i++)
+    EXPECT_EQ(ht.lookup(keys[i]), payloads[i]);
+
+  for (size_t i = 0; i < non_keys.size(); i++)
+    EXPECT_EQ(ht.lookup(non_keys[i]), std::numeric_limits<Payload>::max());
+}
+
+TEST(RMIHashtable, Lookup1_64) {
+  using Key = std::uint64_t;
+  using Payload = std::uint64_t;
+  const std::vector<Key> keys{0, 1, 5, 8, 9, 10};
+  const std::vector<Key> non_keys{2, 3, 4, 6, 7};
+  const std::vector<Payload> payloads{3, 7, 13, 42, 69, 1337};
+
+  RMIHashtable<Key, Payload, 1, 100> ht(keys, payloads);
+  for (size_t i = 0; i < keys.size(); i++)
+    EXPECT_EQ(ht.lookup(keys[i]), payloads[i]);
+
+  for (size_t i = 0; i < non_keys.size(); i++)
+    EXPECT_EQ(ht.lookup(non_keys[i]), std::numeric_limits<Payload>::max());
+}
+
+TEST(RMIHashtable, Lookup2_64) {
+  using Key = std::uint64_t;
+  using Payload = std::uint64_t;
+  const std::vector<Key> keys{0, 1, 5, 8, 9, 10};
+  const std::vector<Key> non_keys{2, 3, 4, 6, 7};
+  const std::vector<Payload> payloads{3, 7, 13, 42, 69, 1337};
+
+  RMIHashtable<Key, Payload, 2, 100> ht(keys, payloads);
+  for (size_t i = 0; i < keys.size(); i++)
+    EXPECT_EQ(ht.lookup(keys[i]), payloads[i]);
+
+  for (size_t i = 0; i < non_keys.size(); i++)
+    EXPECT_EQ(ht.lookup(non_keys[i]), std::numeric_limits<Payload>::max());
+}
+
+TEST(RMIHashtable, Lookup4_64) {
+  using Key = std::uint64_t;
+  using Payload = std::uint64_t;
+  const std::vector<Key> keys{0, 1, 5, 8, 9, 10};
+  const std::vector<Key> non_keys{2, 3, 4, 6, 7};
+  const std::vector<Payload> payloads{3, 7, 13, 42, 69, 1337};
+
+  RMIHashtable<Key, Payload, 4, 100> ht(keys, payloads);
+  for (size_t i = 0; i < keys.size(); i++)
+    EXPECT_EQ(ht.lookup(keys[i]), payloads[i]);
+
+  for (size_t i = 0; i < non_keys.size(); i++)
+    EXPECT_EQ(ht.lookup(non_keys[i]), std::numeric_limits<Payload>::max());
+}
+
+TEST(RMIHashtable, Lookup8_64) {
+  using Key = std::uint64_t;
+  using Payload = std::uint64_t;
+  const std::vector<Key> keys{0, 1, 5, 8, 9, 10};
+  const std::vector<Key> non_keys{2, 3, 4, 6, 7};
+  const std::vector<Payload> payloads{3, 7, 13, 42, 69, 1337};
+
   RMIHashtable<Key, Payload, 8, 100> ht(keys, payloads);
   for (size_t i = 0; i < keys.size(); i++)
     EXPECT_EQ(ht.lookup(keys[i]), payloads[i]);
