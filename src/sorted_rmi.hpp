80252caa5ee49b0fe5183ec02e3d017d451bce74
Dominik Horn <dominik.horn@tum.de>
2021-10-11 19:00:45 +0200

    prepare benchmark to debug RMI performance

--- src/sorted_rmi.hpp
+++ src/sorted_rmi.hpp
@@ -23,51 +23,10 @@ using Key = std::uint64_t;
 using Payload = std::uint64_t;
 
 const size_t gen_dataset_size = 200000000;
-const std::vector<std::int64_t> datasets{
-    dataset::ID::SEQUENTIAL, dataset::ID::UNIFORM, dataset::ID::WIKI,
-    dataset::ID::OSM, dataset::ID::FB};
+const std::vector<std::int64_t> datasets{dataset::ID::SEQUENTIAL};
 
 std::random_device rd;
 std::default_random_engine rng(rd());
-
-static void SortedArrayRangeLookupBinarySearch(benchmark::State& state) {
-  const auto did = static_cast<dataset::ID>(state.range(0));
-  auto dataset = dataset::load_cached<Key>(did, gen_dataset_size);
-
-  state.counters["dataset_size"] = dataset.size();
-  state.SetLabel(dataset::name(did));
-
-  if (dataset.empty()) {
-    // otherwise google benchmark produces an error ;(
-    for (auto _ : state) {
-    }
-    return;
-  }
-
-  // generate payloads
-  auto payloads = dataset;
-  std::shuffle(payloads.begin(), payloads.end(), rng);
-
-  // shuffle for probing
-  auto shuffled_dataset = dataset;
-  std::shuffle(shuffled_dataset.begin(), shuffled_dataset.end(), rng);
-
-  // benchmark
-  size_t i = 0;
-  for (auto _ : state) {
-    while (unlikely(i >= shuffled_dataset.size())) i -= shuffled_dataset.size();
-
-    const auto searched = shuffled_dataset[i++];
-
-    const auto iter =
-        std::lower_bound(dataset.begin(), dataset.end(), searched);
-    const Payload payload = iter < dataset.end()
-                                ? payloads[std::distance(dataset.begin(), iter)]
-                                : std::numeric_limits<Payload>::max();
-    benchmark::DoNotOptimize(payload);
-    full_mem_barrier;
-  }
-}
 
 struct SequentialRangeLookup {
   template <class Predictor>
@@ -212,92 +171,29 @@ static void SortedArrayRangeLookupRMITemplate(benchmark::State& state) {
   size_t i = 0;
   for (auto _ : state) {
     while (unlikely(i >= shuffled_dataset.size())) i -= shuffled_dataset.size();
-
     const auto searched = shuffled_dataset[i++];
 
     const size_t payload_ind = range_lookup(rmi(searched), searched, dataset);
-    const Payload payload = payload_ind < payloads.size()
-                                ? payloads[payload_ind]
-                                : std::numeric_limits<Payload>::max();
-    benchmark::DoNotOptimize(payload);
+    benchmark::DoNotOptimize(payload_ind);
+    // const Payload payload = payload_ind < payloads.size()
+    //                             ? payloads[payload_ind]
+    //                             : std::numeric_limits<Payload>::max();
+    // benchmark::DoNotOptimize(payload);
     full_mem_barrier;
   }
 }
-
-template <size_t SecondLevelModelCount, size_t BucketSize>
-static void BucketsRangeLookupRMI(benchmark::State& state) {
-  const auto did = static_cast<dataset::ID>(state.range(0));
-  auto dataset = dataset::load_cached<Key>(did, gen_dataset_size);
-
-  if (dataset.empty()) {
-    // otherwise google benchmark produces an error ;(
-    for (auto _ : state) {
-    }
-    return;
-  }
-
-  state.counters["dataset_size"] = dataset.size();
-  state.SetLabel(dataset::name(did));
-
-  // generate payloads
-  auto payloads = dataset;
-  std::shuffle(payloads.begin(), payloads.end(), rng);
-
-  // build hashtable
-  std::cout << "building RMIHashtable... " << std::flush;
-  RMIHashtable<Key, Payload, BucketSize, SecondLevelModelCount> ht(dataset,
-                                                                   payloads);
-  std::cout << "done" << std::endl;
-
-  // measure byte sizes
-  state.counters["directory_bytesize"] = ht.directory_byte_size();
-  state.counters["rmi_bytesize"] = ht.rmi_byte_size();
-
-  // shuffle data for probing
-  auto shuffled_dataset = dataset;
-  std::shuffle(shuffled_dataset.begin(), shuffled_dataset.end(), rng);
-
-  // benchmark
-  size_t i = 0;
-  const auto dataset_size = dataset.size();
-  for (auto _ : state) {
-    while (unlikely(i >= dataset_size)) i -= dataset_size;
-
-    const auto searched = shuffled_dataset[i++];
-    const auto payload = ht.lookup(searched);
-    benchmark::DoNotOptimize(payload);
-    full_mem_barrier;
-  }
-}
-
-#define __BENCHMARK_BUCKETS_RANGE_LOOKUP(fun, model_size, bucket_size) \
-  BENCHMARK_TEMPLATE(fun, model_size, bucket_size)->ArgsProduct({datasets});
-
-#define _BENCHMARK_BUCKETS_RANGE_LOOKUP(fun, model_size) \
-  __BENCHMARK_BUCKETS_RANGE_LOOKUP(fun, model_size, 1)   \
-  __BENCHMARK_BUCKETS_RANGE_LOOKUP(fun, model_size, 2)   \
-  __BENCHMARK_BUCKETS_RANGE_LOOKUP(fun, model_size, 4)   \
-  __BENCHMARK_BUCKETS_RANGE_LOOKUP(fun, model_size, 8)
-#define BENCHMARK_BUCKETS_RANGE_LOOKUP(fun)   \
-  _BENCHMARK_BUCKETS_RANGE_LOOKUP(fun, 100)   \
-  _BENCHMARK_BUCKETS_RANGE_LOOKUP(fun, 10000) \
-  _BENCHMARK_BUCKETS_RANGE_LOOKUP(fun, 1000000)
 
 #define BENCHMARK_SORTED_RANGE_LOOKUP_RMI(LookupMethod)                        \
-  BENCHMARK_TEMPLATE(SortedArrayRangeLookupRMITemplate, 100, LookupMethod)     \
+  BENCHMARK_TEMPLATE(SortedArrayRangeLookupRMITemplate, 0, LookupMethod)       \
       ->ArgsProduct({datasets});                                               \
-  BENCHMARK_TEMPLATE(SortedArrayRangeLookupRMITemplate, 10000, LookupMethod)   \
+  BENCHMARK_TEMPLATE(SortedArrayRangeLookupRMITemplate, 100, LookupMethod)     \
       ->ArgsProduct({datasets});                                               \
   BENCHMARK_TEMPLATE(SortedArrayRangeLookupRMITemplate, 1000000, LookupMethod) \
       ->ArgsProduct({datasets});
 
-BENCHMARK_BUCKETS_RANGE_LOOKUP(BucketsRangeLookupRMI);
-
 BENCHMARK_SORTED_RANGE_LOOKUP_RMI(BinaryRangeLookup);
 BENCHMARK_SORTED_RANGE_LOOKUP_RMI(ExponentialRangeLookup);
 BENCHMARK_SORTED_RANGE_LOOKUP_RMI(SequentialRangeLookup);
-
-BENCHMARK(SortedArrayRangeLookupBinarySearch);
 
 }  // namespace _
 
