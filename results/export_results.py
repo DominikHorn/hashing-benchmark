d00fe42aad7363506e20ce935da0ad6ad3fe4f64
Dominik Horn <dominik.horn@tum.de>
2021-10-27 19:23:48 +0200

    reorganize results folder

--- results/export_results.py
+++ results/export_results.py
@@ -7,6 +7,6 @@ def export_json_to_csv(file, result='results.csv'):
         df = pd.json_normalize(data, 'benchmarks')
         df.to_csv(result)
 
-export_json_to_csv("../../benchmark_results.json")
-export_json_to_csv("../../kapil_benchmark_results.json", 'kapil_results.csv')
+export_json_to_csv("../benchmark_results.json")
+export_json_to_csv("../kapil_benchmark_results.json", 'kapil_results.csv')
 
