601a853bc7fda117db1222063a20fc577dea7d58
Dominik Horn <dominik.horn@tum.de>
2021-10-28 23:19:46 +0200

    don't rerun existing benchmark datapoints

---
+++ only_new.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+with open('benchmark_results.json') as results_file:
+    data = json.load(results_file)
+    existing_dp = [dp['name']for dp in data['benchmarks']]
+
+    all_dp = {dp: True for dp in os.popen('cmake-build-release/src/benchmarks --benchmark_list_tests').read().strip().split('\n')}
+
+    for dp in existing_dp:
+        all_dp[dp] = False
+
+    new_dp = [dp[0] for dp in all_dp.items() if dp[1]]
+
+    # verify above is correct
+    for dp in existing_dp:
+        assert(dp not in new_dp)
+        assert(dp in all_dp)
+
+    print('|'.join(new_dp))
--- run.sh
+++ run.sh
@@ -13,5 +13,8 @@ BUILD_DIR="cmake-build-$(echo "${BUILD_TYPE}" | awk '{print tolower($0)}')"
 # Build the target
 ./build.sh ${TARGET} ${BUILD_TYPE}
 
+# Find out which benchmarks are new
+NEW_REGEX=$(python only_new.py)
+
 # Execute the target
-${BUILD_DIR}/src/${TARGET} --benchmark_out=benchmark_results.json --benchmark_out_format=json
+${BUILD_DIR}/src/${TARGET} --benchmark_out=benchmark_results.json --benchmark_out_format=json --benchmark_filter="${NEW_REGEX}" $@
