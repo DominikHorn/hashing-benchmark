9a2471665a0139831e38f82d02b618d80fffcd5f
Dominik Horn <dominik.horn@tum.de>
2021-11-01 22:24:27 +0100

    add script for cleaning benchmark data
    * 'smart' deduplication -> remove worse datapoint on conflict
    * sort data
    * format data

---
+++ cleanup.py
@@ -0,0 +1,22 @@
+import json
+import sys
+
+in_file_path = "benchmark_results.json" if len(sys.argv) < 2 else sys.argv[1]
+out_file_path = "cleaned_results.json" if len(sys.argv) < 3 else sys.argv[2]
+
+with open(in_file_path, 'r', encoding='utf-8') as in_file:
+    # load json
+    raw_json = json.load(in_file)
+
+    # filter out duplicates, keeping best datapoint
+    methods = {}
+    for new_dp in raw_json["benchmarks"]:
+        name = new_dp["name"]
+
+        if name not in methods or float(methods[name]["cpu_time"]) > float(new_dp["cpu_time"]):
+            methods[name] = new_dp
+
+    # write out results
+    raw_json["benchmarks"] = [methods[m] for m in sorted(methods)]
+    with open(out_file_path, 'w', encoding='utf-8') as out_file:
+        json.dump(raw_json, out_file, indent=2)
