a3c4439afcebb4224ecd871540ea397cae833dfa
Dominik Horn <dominik.horn@tum.de>
2021-12-12 14:19:55 +0100

    upgrade exotic_hashing dependency version

--- thirdparty/exotic_hashing.cmake
+++ thirdparty/exotic_hashing.cmake
@@ -4,6 +4,6 @@ set(EXOTIC_HASHING_LIBRARY exotic-hashing)
 FetchContent_Declare(
   ${EXOTIC_HASHING_LIBRARY}
   GIT_REPOSITORY https://github.com/DominikHorn/exotic-hashing.git
-  GIT_TAG 3fbe2fa
+  GIT_TAG 1aaef95 
   )
 FetchContent_MakeAvailable(${EXOTIC_HASHING_LIBRARY})
