beb38d03cf5a7e4f993f668f98d8a8e125551573
kapilvaidya24 <kapilvaidya24@gmail.com>
2022-03-09 22:24:53 -0500

    commiting run.sh

--- run.sh
+++ run.sh
@@ -14,7 +14,9 @@ BUILD_DIR="cmake-build-$(echo "${BUILD_TYPE}" | awk '{print tolower($0)}')"
 ./build.sh ${TARGET} ${BUILD_TYPE}
 
 # Find out which benchmarks are new
-NEW_REGEX=$(python only_new.py)
+# NEW_REGEX=$(python only_new.py)
 
 # Execute the target
-_GLIBCXX_REGEX_STATE_LIMIT=3000 ${BUILD_DIR}/src/${TARGET} --benchmark_out=benchmark_results.json --benchmark_out_format=json --benchmark_filter="${NEW_REGEX}"
+# _GLIBCXX_REGEX_STATE_LIMIT=3000 ${BUILD_DIR}/src/${TARGET} --benchmark_out=benchmark_results.json --benchmark_out_format=json --benchmark_filter="${NEW_REGEX}"
+
+_GLIBCXX_REGEX_STATE_LIMIT=3000 ${BUILD_DIR}/src/${TARGET} --benchmark_out=benchmark_results.json --benchmark_out_format=json 
