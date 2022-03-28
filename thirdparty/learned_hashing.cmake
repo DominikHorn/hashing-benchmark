72661425b2605a61cd5c48b8095fd566c21a6eb8
kapilvaidya24 <kapilvaidya24@gmail.com>
2022-03-28 14:45:33 -0400

    new versions of learned hashing libraries

--- thirdparty/learned_hashing.cmake
+++ thirdparty/learned_hashing.cmake
@@ -4,6 +4,6 @@ set(LEARNED_HASHING_LIBRARY learned-hashing)
 FetchContent_Declare(
   ${LEARNED_HASHING_LIBRARY}
   GIT_REPOSITORY https://github.com/DominikHorn/learned-hashing.git
-  GIT_TAG fd3e05b
+  GIT_TAG 35c8490
   )
 FetchContent_MakeAvailable(${LEARNED_HASHING_LIBRARY})
