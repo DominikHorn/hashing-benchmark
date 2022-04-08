24d9f76ef75332b759699a8794e47c87a71b5527
kapilvaidya24 <kapilvaidya24@gmail.com>
2022-04-08 14:55:08 -0400

    Running update final expt

---
+++ kapil_benchmark_collision.sh
@@ -0,0 +1,10 @@
+
+
+
+# echo "Start Here" 1 0 RMIHash "Model" "Chained" "Balanced" 0 1000000 0 >>logs/collision_stats.out
+# cp src/benchmarks/passive_stats.hpp src/benchmarks/tables.hpp
+# bash run.sh >>logs/collision_stats.out
+
+
+cp src/benchmarks/passive_stats.hpp src/benchmarks/tables.hpp
+bash run.sh >>logs/hash_compute_stats.out
