5ec081088f245697b3f730f30c92102b12ac035a
kapilvaidya24 <kapilvaidya24@gmail.com>
2022-05-09 15:53:21 -0400

    added correct commit tag for learned hashing

--- thirdparty/learned_hashing.cmake
+++ thirdparty/learned_hashing.cmake
@@ -4,6 +4,6 @@ set(LEARNED_HASHING_LIBRARY learned-hashing)
 FetchContent_Declare(
   ${LEARNED_HASHING_LIBRARY}
   GIT_REPOSITORY https://github.com/DominikHorn/learned-hashing.git
-  GIT_TAG f3ca837
+  GIT_TAG 518de16
   )
 FetchContent_MakeAvailable(${LEARNED_HASHING_LIBRARY})
